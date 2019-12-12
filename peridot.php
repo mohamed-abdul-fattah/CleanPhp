81053ec5ae68caa4c86632e93a165d1bdb1f2f0d
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-12-12 21:18:39 +0200

    Peridor watcher

---
+++ peridot.php
@@ -0,0 +1,9 @@
+<?php
+
+use Evenement\EventEmitterInterface;
+use Peridot\Plugin\Watcher\WatcherPlugin;
+
+return function(EventEmitterInterface $emitter) {
+  $watcher = new WatcherPlugin($emitter);
+  $watcher->track(__DIR__ . '/app');
+};
